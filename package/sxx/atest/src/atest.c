32eb90987374949278a073fc4b20af9b813fc4dc
qdk0901 <qdk0901@qq.com>
2015-08-10 14:29:27 +0800

    add simple audio test program

---
+++ package/sxx/atest/src/atest.c
@@ -0,0 +1,108 @@
+/* tinyplay.c
+**
+** Copyright 2011, The Android Open Source Project
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met:
+**     * Redistributions of source code must retain the above copyright
+**       notice, this list of conditions and the following disclaimer.
+**     * Redistributions in binary form must reproduce the above copyright
+**       notice, this list of conditions and the following disclaimer in the
+**       documentation and/or other materials provided with the distribution.
+**     * Neither the name of The Android Open Source Project nor the names of
+**       its contributors may be used to endorse or promote products derived
+**       from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY The Android Open Source Project ``AS IS'' AND
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+** ARE DISCLAIMED. IN NO EVENT SHALL The Android Open Source Project BE LIABLE
+** FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+** DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+** SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+** CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+** LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+** OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+** DAMAGE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
+
+#include "asoundlib.h"
+
+#define SAMPLE_RATE 44100
+#define CHANNEL 2
+#define BUFFER_SIZE (1024)
+
+unsigned short buffer[BUFFER_SIZE];
+
+
+void generate_sine(int freq, int volume)
+{
+    int i = 0;
+    for (i = 0; i < BUFFER_SIZE / CHANNEL; i++) {
+        double x = i * 2 * 3.1415926 / (SAMPLE_RATE / freq);
+        
+        unsigned short data = (unsigned short) (volume * sin(x) + 32768);
+        if (CHANNEL == 1) {
+            buffer[i] = data;
+        } else {
+            buffer[2 * i] = data;
+            buffer[2 * i + 1] = data;
+        }
+    }
+}
+
+int atest()
+{
+    struct pcm_config config;
+    struct pcm *pcm_out;
+    unsigned int card = 0;
+    unsigned int device = 0;
+    unsigned int channels = 2;
+    unsigned int rate = 44100;
+    unsigned int bits = 16;
+    unsigned int period_size = 1024;
+    unsigned int period_count = 4;
+    
+    config.channels = channels;
+    config.rate = rate;
+    config.period_size = period_size;
+    config.period_count = period_count;
+    config.format = PCM_FORMAT_S16_LE;
+    config.start_threshold = 0;
+    config.stop_threshold = 0;
+    config.silence_threshold = 0;
+       
+    pcm_out = pcm_open(card, device, PCM_OUT, &config);
+   	if (!pcm_out || !pcm_is_ready(pcm_out)) {
+        printf("Unable to open PCM device %u (%s)\n",
+                device, pcm_get_error(pcm_out));
+        return 0;
+    }
+       
+    generate_sine(2000, 10000);
+    
+    while (1) {
+        if (!pcm_write(pcm_out, buffer, BUFFER_SIZE)) {
+            //printf("sucess in to out: %d\n", BUFFER_SIZE);	
+        } else {
+            break;
+        }
+    }
+    
+    return 0;
+}
+int main()
+{
+    
+    atest();
+    return 0;
+}
+
